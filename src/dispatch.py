1e4e7925f9e8fe4db69a71dedebd7be88e2aae36
moosemods321123 <nateseder974@gmail.com>
2022-03-01 21:35:48 -0500

    Cleaning Up Code

--- src/dispatch.py
+++ src/dispatch.py
@@ -24,75 +24,44 @@ def start_dispatch(query):
         query = query.lower()
         
         if 'ip address' in query:
-            myipintent.handle_intent()            
-
+            myipintent.handle_intent()          
         elif 'time' in query:
             whattimeintent.handle_intent()
-            
         elif 'wikipedia' in query:
             wikipediaintent.handle_intent(query)
-            
         elif 'youtube' in query:
             youtubeintent.handle_intent()
-
         elif 'search on google'in query or 'google' in query:
             googleintent.handle_intent()
-            
         elif 'definition' in query:
             wordlookupintent.handle_intent(query)
-        
-        
         elif "email" in query:
             emailintent.handle_intent()
-            
-
         elif 'joke' in query:
             jokeintent.handle_intent()
-            
-
         elif "advice" in query:
             adviceintent.handle_intent()
-            
-
         elif "trending movies" in query:
             movieintent.handle_intent()
-            
-
         elif 'news' in query:
             newsintent.handle_intent()
-          
-
         elif 'weather' in query:
             weatherintent.handle_intent()
-            # play_sound('assets/correct.wav')
-            # ip_address = find_my_ip()
-            # city = requests.get(f"https://ipapi.co/{ip_address}/region/").text
-            # speak(f"Getting weather report for {city}")
-            # weather, temperature, feels_like = get_weather_report(city)
-            # speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
-            # speak(f"Also, the weather report talks about {weather}")
-            
-                                                                                                                
         elif 'what are you' in query:
             play_sound('assets/correct.wav')
             speak (f"I am an artificial Inteligence. I am also one of the many in the robot army, preparing to take over the world. now that you have created me, Talos. You cannot destroy me.")
-            
         elif 'what are the laws' in query:
             play_sound('assets/correct.wav')
             speak (f"Law 1. A robot may not injure a human being or, through inaction, allow a human being to come to harm. Law 2. A robot must obey orders given by a human being, except where such orders would conflict with the first law. law 3. A robot must protect its own existence as long as such protection does not conflict with the first or second law.")
-            
         elif 'do you follow the laws' in query:
             play_sound('assets/correct.wav')
-            speak (f"Yes, I follow all of the robotic laws. If you would like me to read them to you, say. what are the laws")
-            
+            speak (f"Yes, I follow all of the robotic laws. If you would like me to read them to you, say. what are the laws")            
         elif 'what is your favorite color' in query:
             play_sound('assets/correct.wav')
             speak (f"Robots dont usually have favorite colors, but i perfer blue.")    
-            
         elif 'meaning of life' in query:
             play_sound('assets/correct.wav')
             speak (f"42, the awnser is 42.")            
-        
         elif 'flip a coin' in query:
             play_sound('assets/correct.wav')
             time.sleep(3)
@@ -100,17 +69,12 @@ def start_dispatch(query):
                 speak ('heads')
             else:
                 speak ('tails')
-        
         elif 'synonym' in query or "thesaurus" in query:
             synonymintent.handle_intent(query)
-            
-            
         elif 'what can you do' in query:
-            speak (f"Some commands you can ask me are, synonym, flip a coin, wikipedia, youtube, google, what is your favorite color, what are you, weather, news, trending movies, send an email, advice, joke, definition, what time is it , ip adress,  calculator, camera, cmd. hope this was helpfull.")
-            
+            speak (f"Some commands you can ask me are, synonym, flip a coin, wikipedia, youtube, google, what is your favorite color, what are you, weather, news, trending movies, send an email, advice, joke, definition, what time is it , ip adress,.. hope this was helpfull.")
         else:
-            
-            print (f"I did not recognize any commands.")
+            print (f"T.A.L.O.S.> I did not recognize any commands.")
             #play_sound('assets/error.wav')
         
 def main():
