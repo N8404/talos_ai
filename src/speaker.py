5daed2bd9ddfd6cc18f94c12e206581ec111a509
N8404 <98066998+N8404@users.noreply.github.com>
2022-03-10 21:39:11 -0500

    T.A.L.O.S. now works as a raspberry file!

--- src/speaker.py
+++ src/speaker.py
@@ -1,19 +1,18 @@
 from wave import WAVE_FORMAT_PCM
 import pyttsx3
-import wave
-import pyaudio
-
-engine = pyttsx3.init('sapi5')
+from playsound import playsound
+engine = pyttsx3.init()
 
 # Set Rate
 engine.setProperty('rate', 190)
 
-# Set Volume
-engine.setProperty('volume', 1.0)
+# # Set Volume
+engine.setProperty('volume', 1.0) 
 
-# Set Voice (Female)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+ # Set Voice (Female)
+#voices = engine.getProperty('voices')
+#engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice','english_rp')
 
 def speak(text):
     """Used to speak whatever text is passed to it"""
@@ -21,33 +20,11 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def play_sound (file_name):
-    chunk = 1024  
-
-    #open a wav format music  
-    f = wave.open(file_name,"rb")  
-    #instantiate PyAudio  
-    p = pyaudio.PyAudio()  
-    #open stream  
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                    channels = f.getnchannels(),  
-                    rate = f.getframerate(),  
-                    output = True)  
-    #read data  
-    data = f.readframes(chunk)  
-
-    #play stream  
-    while data:  
-        stream.write(data)  
-        data = f.readframes(chunk)  
-
-    #stop stream  
-    stream.stop_stream()  
-    stream.close()  
-
-    #close PyAudio  
-    p.terminate()
-    
+def play_sound(file_name):
+    """Plays a sound."""
+    pass
+    #playsound(file_name)
+        
 def main():
     print ("No Default Functionality-Import From An Existing script.")   
     
